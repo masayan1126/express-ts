19136f9d69c96fdbb12f55865372be57a15d54c2
masayan1126 <masa199311266@gmail.com>
2023-09-18 15:24:04 +0900

    Add

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/", async (req: Request, res: Response) => {
   return res.status(200).send({
-    message: "Hello World!",
+    message: "Hello World! cloud run",
   });
 });
 
